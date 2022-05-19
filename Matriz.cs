9cb08fa7baec6cc6d7fea06b55f449c25fbc9b01
AnaCarlaG <acarlagomes250@gmail.com>
2022-05-19 16:44:29 -0300

    variação dos números e parada da variação

--- Matriz.cs
+++ Matriz.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Globalization;
 
 namespace CadeiaMarkov
 {
@@ -30,10 +31,10 @@ namespace CadeiaMarkov
         {
             StreamReader reader = new StreamReader(arquivo);
             int linha = int.Parse(reader.ReadLine());
-            double[] vetor= new double[linha];
+            double[] vetor = new double[linha];
             for (int i = 0; i < linha; i++)
             {
-                    vetor[i] = Convert.ToDouble(reader.ReadLine());
+                vetor[i] =  Convert.ToDouble(reader.ReadLine());
             }
             return vetor;
         }
@@ -95,21 +96,101 @@ namespace CadeiaMarkov
             return matrizC;
         }
 
-        public double[] TransformVector(double[] vetor, double[,] matriz){
+        public void TransformMatrizVector(double[] vetor, double[,] matriz, double tolerancia, bool equiprovavel)
+        {
             var len = matriz.GetLength(1);
             double[] vetorResultante = new double[len];
             for (int linha = 0; linha < len; linha++)
             {
                 for (int coluna = 0; coluna < len; coluna++)
                 {
-                    if(vetor[coluna] != 0){
-                        vetorResultante[linha] += 1*matriz[coluna,linha];
-                    }else{
-                        vetorResultante[linha] +=vetorResultante != null ? matriz[coluna,linha]*0:vetorResultante[linha];
+                    if (vetor[coluna] != 0)
+                    {
+                        vetorResultante[linha] += 1 * matriz[coluna, linha];
+                    }
+                    else
+                    {
+                        vetorResultante[linha] += vetorResultante != null ? matriz[coluna, linha] * 0 : vetorResultante[linha];
                     }
                 }
             }
-            return vetorResultante;
+            Variacao(vetor, vetorResultante, matriz, tolerancia, equiprovavel);
+        }
+
+        public void Variacao(double[] vetor, double[] vetorResultante, double[,] matriz, double tolerancia, bool equiprovavel)
+        {
+            var len = matriz.GetLength(1);
+            double[] vetorComparativo = new double[vetorResultante.Count()];
+            int iteracao = 1;
+            bool converg = false;
+            while (!converg)
+            {
+                if (!equiprovavel)
+                {
+                    for (int i = 0; i < vetor.Count(); i++)
+                    {
+                        if (vetor[i] != 0)
+                        {
+                            vetor[i] = 0;
+                            if (i == vetor.Count())
+                            {
+                                vetor[0] = 1;
+                                break;
+                            }
+                            else
+                            {
+                                vetor[i + 1] = 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+                for (int linha = 0; linha < len; linha++)
+                {
+                    for (int coluna = 0; coluna < len; coluna++)
+                    {
+                        if (vetor[coluna] != 0)
+                        {
+                            vetorResultante[linha] += 1 * matriz[coluna, linha];
+                        }
+                        else
+                        {
+                            vetorResultante[linha] += vetorResultante != null ? matriz[coluna, linha] * 0 : vetorResultante[linha];
+                        }
+                    }
+                }
+
+                PrintVetor(vetorResultante, tolerancia.ToString().Substring(tolerancia.ToString().LastIndexOf(",") + 1).Length);
+                converg = Teste(vetorComparativo, vetorResultante, tolerancia);
+                if (converg)
+                {
+                    PrintVetor(vetorResultante, tolerancia.ToString().Substring(tolerancia.ToString().LastIndexOf(",") + 1).Length);
+                    Console.WriteLine("Quantidade iterações: " + iteracao);
+                } else{
+                    vetorComparativo = vetorResultante;
+                }
+                iteracao++;
+            }
+        }
+        public bool Teste(double[] vetorComparativo, double[] vetorResultante, double tolerancia)
+        {
+            for (int j = 0; j < vetorComparativo.Count(); j++)
+            {
+                var result = Math.Sqrt(Math.Pow(Math.Round(vetorComparativo[j], 4) - Math.Round(vetorResultante[j], 4), 2));
+                if (result < tolerancia)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void PrintVetor(double[] vetorResultante ,int casasDecimais)
+        {
+            for (int j = 0; j < vetorResultante.Count(); j++)
+            {
+                Console.Write(Math.Round(vetorResultante[j],casasDecimais) + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -17,8 +17,10 @@ namespace CadeiaMarkov
             var path3 = Path.Combine(Environment.CurrentDirectory, "Input", "vetorStart.txt");
 
             Matriz matriz = new Matriz();
-            double[,] outraMatriz = matriz.Soma(matriz.Read_Matriz(path,true),matriz.Read_Matriz(path2,false));
-           var vetor= matriz.TransformVector(matriz.Read_Vetor(path3), outraMatriz);
+            double[,] outraMatriz = matriz.Soma(matriz.Read_Matriz(path, true), matriz.Read_Matriz(path2, false));
+            matriz.TransformMatrizVector(matriz.Read_Vetor(path3), outraMatriz, 0.0000001, true);
+
+            
         }
     }
 }
